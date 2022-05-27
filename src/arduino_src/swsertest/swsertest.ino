9d72da773edc5255689c853d2f68db65daaae0a3
woodcol <gp@woodcol.com>
2022-05-27 19:14:45 +0800

    软串口转硬串口

---
+++ src/arduino_src/swsertest/swsertest.ino
@@ -0,0 +1,29 @@
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial swSer(13, 14, false, 256);
+
+void setup() {
+  Serial.begin(115200);
+  swSer.begin(115200);
+
+
+  pinMode(12, OUTPUT); 
+  delay(1);
+  digitalWrite(12, HIGH);
+  Serial.println("\nSoftware serial test started");
+
+  delay(1);
+
+}
+
+void loop() {
+  while (swSer.available() > 0) {
+    Serial.write(swSer.read());
+  }
+  delay(1);
+  while (Serial.available() > 0) {
+    swSer.write(Serial.read());
+  }
+  delay(1);
+}
